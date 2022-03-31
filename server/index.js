d38d064eab728cfb11c2bee79573d80a2208abc0
fqhd <fahd31256@gmail.com>
2022-03-31 18:59:21 +0100

    fix: saving entries to database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import Nedb from 'nedb';
 const app = express();
+const database = new Nedb('database.db');
+
+database.loadDatabase();
 
 // Express Configuration
 app.use(express.json({limit: '1mb'}));
 app.use(cors());
 app.post('/data', (request, response) => {
-	console.log(request.body);
+	const entry = request.body;
+	database.insert(entry);
 });
-
 
 // Starting the server
 app.listen(3000, () => {
