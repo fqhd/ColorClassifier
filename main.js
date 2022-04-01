18360f992a4f20168f55cba3ac7647fd21cedabd
fqhd <fahd31256@gmail.com>
2022-04-01 20:46:13 +0100

    fix: save data to firebase

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
 const canvasElement = document.getElementById('canvas');
 const ctx = canvasElement.getContext('2d');
 let r, g, b;
-const SERVER_IP = 'http://185.239.239.108:3000';
+let database;
 
-changeCanvasColor();
-
-// Setting up the buttons
-for(let i = 0; i < 8; i++){
-	const buttonID = 'button' + (i+1);
-	document.getElementById(buttonID).onclick = buttonFunc;
+function main(){
+	changeCanvasColor();
+	setupButtons();
+	connectToFirebase();
 }
-document.getElementById('skipButton').onclick = changeCanvasColor;
+
+function connectToFirebase(){
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: "AIzaSyDdX6Unz9iNFXV1KUVo80GrJ3Coaq8q5PA",
+		authDomain: "colorclassifier-110e3.firebaseapp.com",
+		projectId: "colorclassifier-110e3",
+		storageBucket: "colorclassifier-110e3.appspot.com",
+		messagingSenderId: "686401618905",
+		appId: "1:686401618905:web:0f111c3f2bf8ebd2b2cfc8"
+	};
+	firebase.initializeApp(firebaseConfig);
+	const db = firebase.firestore();
+	database = db.collection('entries');
+}
+
+function setupButtons(){
+	for(let i = 0; i < 8; i++){
+		const buttonID = 'button' + (i+1);
+		document.getElementById(buttonID).onclick = buttonFunc;
+	}
+	document.getElementById('skipButton').onclick = changeCanvasColor;
+}
 
 function buttonFunc(param){
 	const buttonText = param.srcElement.innerText;
-
-	// Send canvas color and buttonText to server
-	sendDataToServer(buttonText);
-
+	saveToDatabase(buttonText);
 	changeCanvasColor();
 }
 
-function sendDataToServer(buttonText){
-	const obj = {
-		label: buttonText,
-		r, g, b
+function saveToDatabase(buttonText){
+	const data = {
+		r, g, b,
+		label: buttonText
 	};
-	const options = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(obj)
-	};
-	fetch(SERVER_IP + '/data', options);
+	database.add(data);
 }
 
 function changeCanvasColor(){
@@ -47,3 +57,5 @@ function changeCanvasColor(){
 function rgbToString(r, g, b){
 	return `rgb(${r}, ${g}, ${b})`;
 }
+
+main();
